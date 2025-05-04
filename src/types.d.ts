3e43b560ee6a7d2fe4afe861dc2de873ac475474
Adam <adam@haglund.dev>
2025-05-04 19:56:14 +0200

    replace `pkg` with custom node SEA plugin

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,22 @@
-declare global {
-  const PKG_VERSION: string
-}
+declare const PKG_VERSION: string
 
-export {}
+declare module "postject" {
+  import type Buffer from "node:buffer"
+
+  export function inject(
+    execPath: string,
+    resourceName: "NODE_SEA_BLOB" | string,
+    resource: Buffer | ArrayBuffer, // TODO: maybe buffer?
+    options?: {
+      /** @default __POSTJECT */
+      machoSegmentName?: string
+      /** @default false */
+      overwrite?: boolean
+      /** @default POSTJECT_SENTINEL_fce680ab2cc467b6e072b8b5df1996b2 */
+      sentinelFuse?:
+        | "NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2"
+        | "POSTJECT_SENTINEL_fce680ab2cc467b6e072b8b5df1996b2"
+        | string
+    },
+  ): Promise<void>
+}
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,194 @@
+import fs from "node:fs"
+import fsp from "node:fs/promises"
+import path from "node:path"
+
+import { cache } from "empathic/package"
+import { inject } from "postject"
+import { x } from "tinyexec"
 import { defineConfig } from "tsdown"
+import { createUnplugin } from "unplugin"
 
 import pkgJson from "./package.json" with { type: "json" }
+
+const cacheDir = path.relative(
+  process.cwd(),
+  cache("unplugin-node-sea", { create: true })!,
+)
+const configPath = path.join(cacheDir, "sea.json")
+const blobPath = path.join(cacheDir, "sea.blob")
+
+const cleanCacheDir = () => {
+  fs.rmSync(cacheDir, { recursive: true, force: true })
+  fs.mkdirSync(cacheDir, { recursive: true })
+}
+
+const executableExists = (filePath: string) => {
+  if (
+    // If on windows
+    process.platform === "win32" &&
+    // and the file we're looking for isn't specifying .exe extension
+    !path.basename(filePath).includes(".") &&
+    // also check if the file exists with .exe
+    fs.existsSync(`${filePath}.exe`)
+  ) {
+    return true
+  }
+
+  return fs.existsSync(filePath)
+}
+
+const findInPath = (name: string): string | null => {
+  const pathString =
+    process.platform === "win32"
+      ? (process.env.Path ?? process.env.PATH)
+      : process.env.PATH
+  const pathDirs = pathString?.split(path.delimiter) ?? []
+
+  for (const dir of pathDirs) {
+    const fullPath = path.join(dir, name)
+    if (!executableExists(fullPath)) continue
+
+    return fullPath
+  }
+
+  return null
+}
+
+type SeaConfig = {
+  main: string
+  output: string
+  disableExperimentalSEAWarning?: boolean
+  useSnapshot?: boolean
+  useCodeCache: boolean
+  assets?: Record<string, string>
+}
+
+type Options = {
+  name?: string
+  codeCache?: boolean
+  v8Snapshot?: boolean
+}
+
+const seaPlugin = createUnplugin<Options | undefined>(
+  ({ name, codeCache = true, v8Snapshot = false } = {}) => {
+    let execName =
+      name ?? (pkgJson.name.includes("/") ? pkgJson.name.split("/")[1] : pkgJson.name)
+    if (process.platform === "win32") {
+      execName += ".exe"
+    }
+    const execPath = path.join(cacheDir, execName)
+
+    let nodeExecPromise: Promise<void> | null = null
+
+    return {
+      name: "unplugin-node-sea",
+      enforce: "post",
+
+      buildStart: async () => {
+        cleanCacheDir()
+
+        const { promise, resolve } = Promise.withResolvers<void>()
+        nodeExecPromise = promise
+
+        await fsp.copyFile(process.execPath, execPath)
+
+        // Remove exec signing
+        if (process.platform === "win32" && findInPath("signtool") != null) {
+          await x("signtool", ["remove", "/s", execName], {
+            nodeOptions: { cwd: path.resolve(cacheDir) },
+          })
+        }
+        // todo: remove signing on more platforms
+
+        resolve()
+      },
+
+      // todo: more bundlers
+
+      rolldown: {
+        outputOptions(options) {
+          if (options.format !== "commonjs" && options.format !== "cjs") {
+            this.error("Node SEA only supports CommonJS. Set `output.format` to `cjs`.")
+          }
+
+          if (codeCache) {
+            if (options.inlineDynamicImports === false) {
+              this.warn(
+                "Node SEA only supports compile cache without `import()` statements. If your code has any, either disable `codeCache` or enable `output.inlineDynamicImports`.",
+              )
+            }
+
+            options.inlineDynamicImports = true
+          }
+
+          return options
+        },
+
+        async writeBundle(_, output) {
+          const outDir = (this as never as { outputOptions: { dir: string } })
+            .outputOptions.dir
+          let entryPoint: string | null = null
+          const assets = new Map<string, string>()
+
+          for (const file of Object.values(output)) {
+            // TODO: check that no file contains `import()`
+
+            if (file.type === "chunk" && file.isEntry) {
+              entryPoint = path.join(outDir, file.fileName)
+            } else {
+              if (codeCache && file.type === "chunk" && file.isDynamicEntry) {
+                // TODO: error
+                this.warn(
+                  `Found a dynamic chunk with codeCache enabled. This is not supported by Node.`,
+                )
+              }
+              assets.set(file.fileName, path.join(outDir, file.fileName))
+            }
+          }
+          if (entryPoint == null) throw new Error("No entry point found")
+
+          const distExecDir = path.join(path.dirname(outDir), "dist-exec")
+
+          fs.cpSync(entryPoint, path.join(cacheDir, path.basename(entryPoint)))
+
+          // Create SEA config
+          const config = {
+            main: entryPoint,
+            output: blobPath,
+            // Only works without import()
+            // https://nodejs.org/api/single-executable-applications.html#v8-code-cache-support
+            useCodeCache: codeCache,
+            // https://nodejs.org/api/single-executable-applications.html#startup-snapshot-support
+            useSnapshot: v8Snapshot,
+          } satisfies SeaConfig as SeaConfig
+          if (assets.size !== 0) {
+            config.assets = Object.fromEntries(assets)
+            // TODO: test assets
+          }
+
+          this.debug(JSON.stringify(config, null, 2))
+          fs.writeFileSync(configPath, JSON.stringify(config))
+
+          // Generate SEA blob from config
+          await x(process.execPath, ["--experimental-sea-config", configPath])
+          const blob = fs.readFileSync(blobPath)
+
+          // Copy Node executable
+          await nodeExecPromise
+
+          // Inject SEA blob into node exec
+          await inject(execPath, "NODE_SEA_BLOB", blob, {
+            sentinelFuse: "NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2",
+          })
+
+          fs.mkdirSync(distExecDir, { recursive: true })
+          fs.renameSync(execPath, path.join(distExecDir, execName))
+          cleanCacheDir()
+        },
+      },
+    }
+  },
+)
 
 export default defineConfig({
   entry: "src/cli.ts",
@@ -11,9 +199,10 @@ export default defineConfig({
   },
 
   platform: "node",
-  target: "node18",
-  format: "esm",
+  target: "node20",
+  format: "cjs",
+  minify: false,
   fixedExtension: true,
 
-  minify: false,
+  plugins: [seaPlugin.rolldown()],
 })
